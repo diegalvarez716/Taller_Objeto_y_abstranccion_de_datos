e997067f632301631c3cde349ffdb2674565c297
diegalvarez716 <149430145+diegalvarez716@users.noreply.github.com>
2024-02-05 15:48:24 -0400

    Delete laboratorio1 directory

--- laboratorio1/laboratorio1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.laboratorio1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.laboratorio1 to javafx.fxml;
-    exports com.example.laboratorio1;
-}
